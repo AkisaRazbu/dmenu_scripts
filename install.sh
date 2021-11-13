a18c25f6e194e3442576df79b985ba10979ccf13
Akisa <akisa@disroot.org>
2021-11-13 02:45:48 +0100

    fixed

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-mkdir ~/.scripts || echo ".scripts directory already exists  in your home directory" && mv global.sh ~/.scripts && mv exitmenu.sh ~/.scripts
-&& echo -e "\nDone."
+mkdir ~/.scripts || echo ".scripts directory already exists  in your home directory" && mv global.sh ~/.scripts && mv exitmenu.sh ~/.scripts && echo -e "\nDone."
 chmod +x global.sh && chmod +x exitmenu.sh
 mv global.sh ~/.scripts && mv exitmenu.sh ~/.scripts
