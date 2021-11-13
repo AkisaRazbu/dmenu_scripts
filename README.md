6085939535ffef299740cbd3f203c48e5f9d9aaf
Akisa <akisa@disroot.org>
2021-11-13 02:42:15 +0100

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# dmenu_scripts
+## Akisa's dmenu scripts
+
+### Installation
+```
+chmod +x install.sh
+./install.sh
+```
+
+### Usage
+
+<b>Bind ~/.scripts/global.sh to a keybinding in your WM and use that to run the script.</b>
+
---
+++ install.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+mkdir ~/.scripts || echo ".scripts directory already exists  in your home directory" && mv global.sh ~/.scripts && mv exitmenu.sh ~/.scripts
+chmod +x global.sh && chmod +x exitmenu.sh
+mv global.sh ~/.scripts && mv exitmenu.sh ~/.scripts
